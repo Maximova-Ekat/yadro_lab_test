6801b0b40df23392ebf7ff959211e2811912cdda
Maximova-Ekat <Maxi.Kate0@gmail.com>
2025-09-23 00:08:57 +0300

    improve html check

--- agent/tests/test_http_pages.py
+++ agent/tests/test_http_pages.py
@@ -3,27 +3,15 @@ import requests
 
 
 def validate_index_page(resp: requests.Response):
-<<<<<<< HEAD
     content_type = resp.headers.get("Content-Type", "")
     assert "text/html" in content_type, f"Unexpected Content-Type: {content_type}"
 
-=======
-    """Проверка корректности страницы index"""
-    # Проверка заголовков
-    content_type = resp.headers.get("Content-Type", "")
-    assert "text/html" in content_type, f"Unexpected Content-Type: {content_type}"
-
-    # Проверка содержимого
->>>>>>> 0e41ff75e152c7b8adbc3f228e1dafea198d5b8f
     text = resp.text
     assert "<title>Test Page</title>" in text, "Missing or incorrect <title>"
     assert "<h1>Hello from Apache!" in text, "Missing expected header text"
 
 
-<<<<<<< HEAD
 # HTTP тесты для index и 404 страницы
-=======
->>>>>>> 0e41ff75e152c7b8adbc3f228e1dafea198d5b8f
 @pytest.mark.parametrize(
     "path, expected_status, validator",
     [
@@ -32,10 +20,6 @@ def validate_index_page(resp: requests.Response):
     ],
 )
 def test_http_pages(target_config, path, expected_status, validator):
-<<<<<<< HEAD
-=======
-    """HTTP тесты для index и 404 страницы"""
->>>>>>> 0e41ff75e152c7b8adbc3f228e1dafea198d5b8f
     host = target_config["host"]
     port = target_config["http_port"]
     timeout = target_config["http_timeout"]
@@ -46,18 +30,10 @@ def test_http_pages(target_config, path, expected_status, validator):
     except requests.RequestException as e:
         pytest.fail(f"HTTP request to {url} failed: {e}")
 
-<<<<<<< HEAD
-=======
-    # Проверка статус-кода
->>>>>>> 0e41ff75e152c7b8adbc3f228e1dafea198d5b8f
     assert resp.status_code == expected_status, (
         f"{url} returned {resp.status_code}, expected {expected_status}. "
         f"Body snippet: {resp.text[:100]}"
     )
 
-<<<<<<< HEAD
-=======
-    # Дополнительная валидация (для 200 OK)
->>>>>>> 0e41ff75e152c7b8adbc3f228e1dafea198d5b8f
     if validator:
         validator(resp)
